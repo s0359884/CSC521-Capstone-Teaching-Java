5801091c4ef0c1ca6c434ce3410e88b47ede227d
s0359884 <andrewfacada@gmail.com>
2022-04-08 21:18:01 -0400

    Fixed admin login password issue

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
             if($result && mysqli_num_rows($result)>0)
             {
                 $user_data = mysqli_fetch_assoc($result);
-                if($user_data['user_id'] == 1)
+                if($user_data['user_id'] == 1 && $user_data['password'] === $password)
                 {
                     header("Location:admin.php");
                     die;
