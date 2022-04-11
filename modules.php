cfbdf87f75b7d009bfb75584568cfe69bf23b8d4
s0359884 <andrewfacada@gmail.com>
2022-04-11 18:15:41 -0400

    Now works with setup modules

--- modules.php
+++ modules.php
@@ -1,9 +1,22 @@
 <?php
 session_start();
-
 include("connection.php");
 include("functions.php");
 $user_data = check_login($con);
+
+if(isset($_POST['forMenu']))
+{
+    header('location:menu.php');
+}
+
+if(isset($_POST['variables']))
+{
+    header('location: moduleVariables1.php');
+}
+
+
+
+
 
 ?>
 <!doctype html>
@@ -17,6 +30,20 @@ $user_data = check_login($con);
     <link rel="stylesheet" href="styleModules.css">
 </head>
 <body>
+<div id = "box">
+    <form method="post">
+        <div style="font-size: 20px;margin: 10px;color: white;">Modules!</div>
+        <input id="button" type="submit" name = "variables" value="Variables"><br><br>
+        <input id="button" type="submit" name = "arrays" value="Arrays"><br><br>
+        <input id="button" type="submit" name = "dataStructures" value="Data Structures"><br><br>
+        <input id="button" type="submit" name = "ifStatements" value="If-Statements"><br><br>
+        <input id="button2" type="submit" name = "forMenu" value="Back to Menu">
+
+    </form>
+
+</div>
+
+
 
 </body>
 </html>
