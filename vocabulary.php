e1fc7fa8764ec05dd78a94306121adc465af10cf
s0359884 <andrewfacada@gmail.com>
2022-04-18 15:15:11 -0400

    basic vocab page. To be filled

--- vocabulary.php
+++ vocabulary.php
@@ -8,7 +8,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vocabulary!</title>
-    <link rel="stylesheet" href="styleVocabulary.css">
+    <link rel="stylesheet" href="css/styleVocabulary.css">
+    <link rel="icon" href="images/javaIcon.png">
 </head>
 <body>
 
