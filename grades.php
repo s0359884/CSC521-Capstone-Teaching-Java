69bf87efe6c671cd210900dad6b68b9f6d049206
s0359884 <andrewfacada@gmail.com>
2022-04-18 15:17:29 -0400

    To be updated

--- grades.php
+++ grades.php
@@ -14,7 +14,8 @@ $user_data = check_login($con);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Grades!</title>
-    <link rel="stylesheet" href="styleGrades.css">
+    <link rel="stylesheet" href="css/styleGrades.css">
+    <link rel="icon" href="images/javaIcon.png">
 </head>
 <body>
 
--- quizzes.php
+++ quizzes.php
@@ -15,7 +15,8 @@ $user_data = check_login($con);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Quizzes!</title>
-    <link rel="stylesheet" href="styleModules.css">
+    <link rel="stylesheet" href="css/styleModules.css">
+    <link rel="icon" href="images/javaIcon.png">
 </head>
 <body>
 
