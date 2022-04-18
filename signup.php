f59a987725a47c95ce03bcca8a2563e814e77d13
s0359884 <andrewfacada@gmail.com>
2022-04-18 15:11:11 -0400

    New account passwords hashed

--- signup.php
+++ signup.php
@@ -12,11 +12,17 @@ session_start();
         $password = $_POST['password'];
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        if(isset($_POST['login']))
+        {
+            header('location: login.php');
+        }
 
         if(!empty($email) && !empty($password) && !empty($first_name) && !empty($last_name) && !is_numeric($first_name) && !is_numeric($last_name))
         {
             //save to database
-            $query = "insert into users (first_name,last_name,email,password) values ('$first_name','$last_name','$email','$password')";
+            $query = "insert into users (first_name,last_name,email,password) values ('$first_name','$last_name','$email','$hashed_password')";
             mysqli_query($con,$query);
 
             header("Location: login.php");
@@ -44,7 +50,8 @@ session_start();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Signup</title>
-    <link rel="stylesheet" href="styleSignup.css">
+    <link rel="stylesheet" href="css/styleSignup.css">
+    <link rel="icon" href="images/javaIcon.png">
 </head>
 <body>
 
@@ -52,16 +59,17 @@ session_start();
 <div id="box">
 
     <form method="post">
-        <div style="font-size: 20px;margin: 10px;color: white;">Signup</div>
+        <h1>The Java Toolbox</h1>
+        <h2>Signup</h2>
 
         <input id="text" type="text" name="email" placeholder="Email..."><br><br>
         <input id="text" type="password" name="password" placeholder="Password..."><br><br>
         <input id="text" type="text" name="first_name" placeholder="First name..."><br><br>
         <input id="text" type="text" name="last_name" placeholder="Last name..."><br><br>
 
-        <input id="button" type="submit" value="Signup"><br><br>
+        <input id="button" type="submit" value="Signup">
 
-        <a href="login.php">Click to Login</a><br><br>
+        <input id="button2" name="login" type="submit" value="Login">
     </form>
 </div>
 </body>
