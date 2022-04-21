b464d50ff5013478b55a6dbe9b1d95ed6dc93481
s0359884 <andrewfacada@gmail.com>
2022-04-21 11:20:12 -0400

    Shows all grades from user point of view

--- grades.php
+++ grades.php
@@ -4,6 +4,12 @@ session_start();
 include("connection.php");
 include("functions.php");
 $user_data = check_login($con);
+$user_id = $_SESSION['user_id'];
+
+if(isset($_POST['forMenu']))
+{
+    header('location:menu.php');
+}
 ?>
 
 <!doctype html>
@@ -18,6 +24,29 @@ $user_data = check_login($con);
     <link rel="icon" href="images/javaIcon.png">
 </head>
 <body>
+<div id="box">
+
+    <form method="post">
+        <h1>All Grades!</h1>
+
+        <p>
+            <?php
+                $query = "SELECT quiz, grade FROM logs WHERE $user_id = user_id";
+                $result = mysqli_query($con,$query);
+
+                if($result -> num_rows > 0)
+                {
+                    while($row = $result -> fetch_assoc())
+                    {
+                        echo "Quiz: ". $row["quiz"]."<br>". "Grade: ". $row["grade"]."<br>"."<br>";
+                    }
+                }
+            ?>
+        </p>
+
+        <input id="button2" type="submit" name = "forMenu" value="Back to Menu">
+    </form>
+
 
 </body>
 </html>
