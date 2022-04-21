b25a063feee0aaf92e588767d9f7f5c04748a4db
s0359884 <andrewfacada@gmail.com>
2022-04-21 19:53:28 -0400

    working vocabulary

--- vocabulary.php
+++ vocabulary.php
@@ -1,4 +1,14 @@
 <?php
+session_start();
+
+include("connection.php");
+include("functions.php");
+$user_data = check_login($con);
+
+if(isset($_POST['forMenu']))
+{
+    header('location:menu.php');
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,6 +22,33 @@
     <link rel="icon" href="images/javaIcon.png">
 </head>
 <body>
+
+<div id="box">
+
+    <form method="post">
+        <h1>Vocabulary!</h1>
+
+        <p>
+            <?php
+
+            $query = "SELECT * FROM vocabulary";
+            $result = mysqli_query($con,$query);
+
+            if($result -> num_rows > 0)
+            {
+                while($row = $result -> fetch_assoc())
+                {
+                    echo $row["vocab_name"]."<br>".  $row["definition"]."<br>"."<br>";
+                }
+            }
+
+
+
+            ?>
+        </p>
+
+        <input id="button2" type="submit" name = "forMenu" value="Back to Menu">
+    </form>
 
 </body>
 </html>
